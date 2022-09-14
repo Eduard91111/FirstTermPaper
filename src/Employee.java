07035457a931cd630262d453523c4f9b2b25bca8
Эдуард <Edyard911@yandex.ru>
2022-09-14 19:14:29 +0400

    Не пойму как теперь сложить зарплату в Main?

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    private String FIO;
+    private int Department;
+    private float Salary;
+    private static int Id;
+    public Employee(String fio, int department, float salary) {
+        this.FIO = fio;
+        this.Department = department;
+        this.Salary = salary;
+        Id = 0;
+        Id++;
+    }
+
+    public int getId() { return Id; }
+    public String getFIO() { return FIO; }
+    public int getDepartment() { return Department; }
+    public float getSalary() { return Salary; }
+
+    public void SetFIO(String fio) { this.FIO = fio; }
+    public void SetDepartment(int department) { this.Department = department; }
+    public void SetSalary(float sal) { this.Salary = sal; }
+
+    @Override
+    public String toString() {
+        return Id++ + " Ф.И.О.: " + FIO + " Отдел: " + Department + " Зарплата: " + Salary;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        Employee[] empl = new Employee[10];
+        empl[0] = new Employee("ФИО", 1, 31000f);
+        empl[1] = new Employee("ФИО", 2, 32500f);
+        empl[2] = new Employee("ФИО", 3, 25000f);
+        empl[3] = new Employee("ФИО", 4, 29000f);
+        empl[4] = new Employee("ФИО", 5, 50000f);
+        empl[5] = new Employee("ФИО", 6, 80000f);
+        empl[6] = new Employee("ФИО", 7, 120000f);
+        empl[7] = new Employee("ФИО", 8, 20000f);
+        empl[8] = new Employee("ФИО", 9, 48000f);
+        empl[9] = new Employee("ФИО", 10, 52000f);
+        for (Employee e : empl) {
+            System.out.println(e);
+        }
     }
 }
